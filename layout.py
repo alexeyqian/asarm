c8c10e31c89554220ee0a8ac940b4c3198bec736
Alexey <alexeyqian@gmail.com>
2026-04-25 10:24:14 +0900

    add layout

---
+++ layout.py
@@ -0,0 +1,13 @@
+class Layout:
+    def __init__(self, base = 0x400000):
+        self.base = base
+        
+    def compute(self, text_size, data_size):
+        text_offset = 64 + 56
+        data_offset = text_offset + text_size
+        return {
+            "text_offset": text_offset,
+            "data_offset": data_offset,
+            "text_base": self.base + text_offset,
+            "data_base": self.base + data_offset
+        }
